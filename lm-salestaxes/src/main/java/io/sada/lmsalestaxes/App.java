798aeca87deef4a29d1921e5861e056952540210
Enrico Sada <enrico@sada.io>
2018-03-02 19:53:35 +0100

    gen from template

---
+++ lm-salestaxes/src/main/java/io/sada/lmsalestaxes/App.java
@@ -0,0 +1,13 @@
+package io.sada.lmsalestaxes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
