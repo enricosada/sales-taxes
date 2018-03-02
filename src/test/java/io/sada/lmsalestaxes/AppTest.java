b89dffde0b82285e76025294d1028d9d4439104e
Enrico Sada <enrico@sada.io>
2018-03-02 22:21:30 +0100

    upgrade to junit 4

--- src/test/java/io/sada/lmsalestaxes/AppTest.java
+++ src/test/java/io/sada/lmsalestaxes/AppTest.java
@@ -1,33 +1,11 @@
 package io.sada.lmsalestaxes;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    @Test
     public void testApp() {
         assertTrue(true);
     }
