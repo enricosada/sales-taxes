72061d3bfee1120d0ec7e4fda48cf2539013f2cc
Enrico Sada <enrico@sada.io>
2018-03-03 12:21:25 +0100

    check purchasing an item has the unity price as total

--- src/main/java/io/sada/lmsalestaxes/MyApp.java
+++ src/main/java/io/sada/lmsalestaxes/MyApp.java
@@ -1,14 +1,47 @@
 package io.sada.lmsalestaxes;
 
+import java.math.BigDecimal;
+
 public class MyApp {
+    private OrderItem item;
+
     public void purchase(int quantity, String product, String unitaryPrice) {
+        this.purchase(quantity, product, new BigDecimal(unitaryPrice));
+    }
+
+    public void purchase(int quantity, String product, BigDecimal unitaryPrice) {
+        this.item = new OrderItem(quantity, product, unitaryPrice);
     }
 
     public String[] getReceipt() {
         return new String[]{
-                "1 book: 12.49",
+                "1 book: " + this.item.getUnitaryPrice().toString(),
                 "Sales Taxes: 0",
-                "Total: 12.49"
+                "Total: " + this.item.getUnitaryPrice().toString()
         };
+    }
+
+    private final class OrderItem {
+        private final int quantity;
+        private final String product;
+        private final BigDecimal unitaryPrice;
+
+        public OrderItem(int quantity, String product, BigDecimal unitaryPrice) {
+            this.quantity = quantity;
+            this.product = product;
+            this.unitaryPrice = unitaryPrice;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public String getProduct() {
+            return product;
+        }
+
+        public BigDecimal getUnitaryPrice() {
+            return unitaryPrice;
+        }
     }
 }
--- src/test/java/io/sada/lmsalestaxes/ScenarioTest.java
+++ src/test/java/io/sada/lmsalestaxes/ScenarioTest.java
@@ -1,11 +1,17 @@
 package io.sada.lmsalestaxes;
 
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.math.BigDecimal;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(JUnitQuickcheck.class)
 public class ScenarioTest {
 
     @Test
@@ -22,4 +28,20 @@ public class ScenarioTest {
                 "Total: 12.49"
         }, receipt);
     }
+
+    @Property
+    public void oneItemAnyPriceIsAlwaysTheTotal(BigDecimal price) {
+        MyApp app = new MyApp();
+
+        app.purchase(1, "book", price);
+
+        String[] receipt = app.getReceipt();
+
+        assertArrayEquals(new String[]{
+                "1 book: " + price.toString(),
+                "Sales Taxes: 0",
+                "Total: " + price.toString()
+        }, receipt);
+    }
+
 }
