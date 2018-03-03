a3006f0bfa09b14ca0c19a3fbb1803393d8f71ef
Enrico Sada <enrico@sada.io>
2018-03-03 11:42:21 +0100

    first red

---
+++ src/test/java/io/sada/lmsalestaxes/ScenarioTest.java
@@ -0,0 +1,34 @@
+package io.sada.lmsalestaxes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ScenarioTest {
+
+    @Test
+    public void oneItem() {
+        MyApp app = new MyApp();
+
+        app.purchase(1, "book", "12.49");
+
+        String[] receipt = app.getReceipt();
+
+        assertArrayEquals(new String[]{
+                "1 book: 12.49",
+                "Sales Taxes: 0",
+                "Total: 12.49"
+        }, receipt);
+    }
+
+    private class MyApp {
+        public void purchase(int quantity, String product, String unitaryPrice) {
+        }
+
+        public String[] getReceipt() {
+            return null;
+        }
+    }
+}
